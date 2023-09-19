531b7413a445b9cc453e8540b25f7eb2435619e2
BismaHaroon <125575282+BismaHaroon@users.noreply.github.com>
2023-09-19 20:19:50 +0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,35 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    print("Hello World")
+    return "hello world"
 
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    operation = request.form['operation']
-    num1 = float(request.form['num1'])
-    num2 = float(request.form['num2'])
-    
+@app.route('/calc/<operation>/<int:num1>/<int:num2>')
+def calculate(operation, num1, num2):
     if operation == 'add':
-        result = num1 + num2
-        operation_name = 'ADD'
-    elif operation == 'sub':
-        result = num1 - num2
-        operation_name = 'SUBTRACT'
-    elif operation == 'mult':
-        result = num1 * num2
-        operation_name = 'MULTIPLY'
-    elif operation == 'div':
-        if num2 == 0:
-            return "Error!! Division by zero is INVALID"
-        result = num1 / num2
-        operation_name = 'DIVIDE'
-    else:
-        return "Invalid operation"
-
-    return render_template('result.html', operation=operation_name, num1=num1, num2=num2, result=result)
+        result = num1+num2
+    elif operation == 'subtract':
+        result = num1-num2
+    elif operation == 'multiply':
+        result = num1*num2
+    elif operation == 'divide':
+        result = num1/num2
+    
+    return str(result)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0')
