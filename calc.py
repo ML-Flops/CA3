82a7474be1664f302b507b9980ad39f22616d690
Saamiya <125141049+SaamiyaMariam@users.noreply.github.com>
2023-09-19 19:35:18 +0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,21 +1,35 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-from calculator import Subtract,Multiply,Add,Div
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-@app.route('/calc/<operation>/<int:num1>/<int:num2>')
-def calculate(operation, num1, num2):
-    if operation == 'add':
-        result = Add(num1, num2)
-    elif operation == 'subtract':
-        result = Subtract(num1, num2)
-    elif operation == 'multiply':
-        result = Multiply(num1, num2)
-    elif operation == 'divide':
-        result = Div(num1, num2)
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    operation = request.form['operation']
+    num1 = float(request.form['num1'])
+    num2 = float(request.form['num2'])
     
-    return str(result)
+    if operation == 'add':
+        result = num1 + num2
+        operation_name = 'ADD'
+    elif operation == 'sub':
+        result = num1 - num2
+        operation_name = 'SUBTRACT'
+    elif operation == 'mult':
+        result = num1 * num2
+        operation_name = 'MULTIPLY'
+    elif operation == 'div':
+        if num2 == 0:
+            return "Error!! Division by zero is INVALID"
+        result = num1 / num2
+        operation_name = 'DIVIDE'
+    else:
+        return "Invalid operation"
+
+    return render_template('result.html', operation=operation_name, num1=num1, num2=num2, result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
